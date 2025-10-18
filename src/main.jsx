00f3f2472e77262a7bdfd1a8851df0dcd64fc005
frandi-dev <frandipradana31@gmail.com>
2025-10-18 19:43:14 +0700

    setup ulang dan tambah bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
 
+// Import Bootstrap CSS & JS
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Router>
